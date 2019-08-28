eaa5c139e2111f834a8bdd66d0e5737ff02da226
Jackmc <jackmc1122@gmail.com>
2019-08-28 14:04:37 -0700

    Add tests for documentSelectors

--- src/selectors/documentSelector.js
+++ src/selectors/documentSelector.js
@@ -1,2 +1,2 @@
 export const getDocumentState = (state) => state.document;
-export const getMarkDown = state => getDocumentState(state).markdown;
+export const getMarkdown = state => getDocumentState(state).markdown;
