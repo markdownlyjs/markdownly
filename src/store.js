816ed925cd857d1b18740e2d315f8eda1d3e7dc2
Jackmc <jackmc1122@gmail.com>
2019-08-29 13:50:57 -0700

    Add persistance

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persist, initState } from './middleware/persist';
 import reducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  initState(),
+  composeEnhancers(applyMiddleware(persist))
 );
